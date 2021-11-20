1544a2724638ccdd2797acaadc5faf7122686869
jhamadhav <jhamadhav28@gmail.com>
2021-11-20 12:24:29 +0530

    lab1 linear search added

---
+++ lab-1/linearSearchByReference.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void linearSearch(int arr[], int* n, int* num, int* index) {
+    for (int i = 0;i < *n;i++) {
+        if (arr[i] == *num) {
+            *index = i;
+            break;
+        }
+    }
+}
+
+int main() {
+    int n = 2;
+    printf("\nEnter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter array elements: ");
+    for (int i = 0;i < n;i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int num;
+    printf("Enter number to search: ");
+    scanf("%d", &num);
+
+    int index = -1;
+    linearSearch(arr, &n, &num, &index);
+
+    if (index == -1) {
+        printf("Number not found!!!");
+    }
+    else {
+        printf("Number found at index: %d", index);
+    }
+    printf("\n\n");
+    return 0;
+}
